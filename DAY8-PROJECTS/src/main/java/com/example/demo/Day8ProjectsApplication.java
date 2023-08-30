57c3e19130115101a5311ebfd692f19ded6d95ca
Dhanusritk <107863345+Dhanusritk@users.noreply.github.com>
2023-08-30 10:11:39 +0530

    Add files via upload

---
+++ DAY8-PROJECTS/src/main/java/com/example/demo/Day8ProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day8ProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day8ProjectsApplication.class, args);
+	}
+
+}
